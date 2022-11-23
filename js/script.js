802dea8b1138a19f8c5e9319bbebad38bb021853
huntersteffner <huntersteffner@gmail.com>
2022-11-23 10:10:16 -0500

    Mobile menu complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+const navToggle = () => {
+    btn.classList.toggle('open')
+    menu.classList.toggle('hidden')
+    menu.classList.toggle('flex')
+}
+btn.addEventListener('click', navToggle)
